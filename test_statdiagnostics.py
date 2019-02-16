45c37d9af8fc0e4c656e5b6b54d7142a33aa4903
dapeter <dapeter@student.ethz.ch>
2019-02-16 11:01:48 +0100

    update

--- test_statdiagnostics.py
+++ test_statdiagnostics.py
@@ -7,17 +7,16 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 def test_statdiagnostics(area, peak_power, expected_data, measured_data, months):
-
+    
+    '''
     #normalize data
     expected_data_normalized = [float(i)/sum(expected_data) for i in expected_data]
     measured_data_normalized = [float(i)/sum(measured_data) for i in measured_data]
+    '''
 
     pv_efficiency = pv_compute_efficiency(area, peak_power)
     print('computed pv_efficiency 0', pv_efficiency)
-
     corrected_expected_data = pv_correct_expected(pv_efficiency, expected_data)
-    print('expected =', expected_data)
-    print('corrected_expected =', corrected_expected_data)
     
     #plot expected data and measured data
     df_rearanged = pd.DataFrame({
@@ -32,29 +31,30 @@ def test_statdiagnostics(area, peak_power, expected_data, measured_data, months)
     print('output =', pv_has_malfunction(area, expected_data, measured_data))
     print('output with correction =', pv_has_malfunction_with_correction(area, peak_power, expected_data, measured_data))
 
-    
+    ''' 
     #plot normalized expected data and normalizde measured data
-    #df_rearanged = pd.DataFrame({
-    #    'reference normalized' : expected_data_normalized,
-    #    'measured normalized' : measured_data_normalized
-    #    },index = months
-    #)
-    #df_rearanged.plot(kind='bar')
-    #plt.show()
+    df_rearanged = pd.DataFrame({
+        'reference normalized' : expected_data_normalized,
+        'measured normalized' : measured_data_normalized
+        },index = months
+    )
+    df_rearanged.plot(kind='bar')
+    plt.show()
     print('normalized data: output =', pv_has_malfunction(1, expected_data_normalized, measured_data_normalized))
     
-    #plot normalized expected data and perturbed normalizde measured data
-    #perturb data:
-    #measured_data_normalized[5] /= 2
-    #measured_data_normalized[6] /= 2
+    plot normalized expected data and perturbed normalizde measured data
+    perturb data:
+    measured_data_normalized[5] /= 2
+    measured_data_normalized[6] /= 2
     
     #df_rearanged = pd.DataFrame({
-    #    'reference normalized + perturbation' : expected_data_normalized,
-    #    'measured normalized + perturbation' : measured_data_normalized
-    #    },index = months
-    #)
-    #df_rearanged.plot(kind='bar')
-    #plt.show()
-    #print('normalized data + perturbation: output =', pv_has_malfunction(1, expected_data_normalized, measured_data_normalized))
+        'reference normalized + perturbation' : expected_data_normalized,
+        'measured normalized + perturbation' : measured_data_normalized
+        },index = months
+    )
+    df_rearanged.plot(kind='bar')
+    plt.show()
+    print('normalized data + perturbation: output =', pv_has_malfunction(1, expected_data_normalized, measured_data_normalized))
+    '''
     return
 
